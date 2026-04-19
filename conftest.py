8db77b76e33d9bd7ba08e65349376ad53a900d6c
anikj <anikjevjenya@gmail.com>
2026-04-19 12:57:14 +0300

    all tests ready

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import sys
 import os
 """pytest tests/ -v"""
+"""pytest tests/ -v --alluredir=allure-results"""
+"""C:\allure\bin\allure.bat serve allure-results"""
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
